3e908e3cb9967081ecd2903cbdfd4857b24baeff
Martin Jensen <maage@dotmaage.dk>
2015-04-23 23:53:54 +0200

    Update to version 6.4.2

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -10,7 +10,9 @@ REGEX_STARTUP = /Server startup in \d+ ms/
 REGEX_FILTER  =
 [
   # when `dbconfig.xml` does not exists when starting up instance
-  /no\ defaultDS\ datasource/
+  /no\ defaultDS\ datasource/,
+  # https://jira.atlassian.com/browse/JRA-42576
+  /\[atlassian\.jira\.tenancy\.PluginKeyPredicateLoader\]\ Could\ not\ read\ tenant\-smart\ pattern\ file\ '\/usr\/local\/atlassian\/jira\/atlassian\-jira\/WEB\-INF\/classes\/tenant\-smart\-patterns\.txt'\ \(using\ defaults\)/
 ].inject { |*args| Regexp.union(*args) }
 
 Dir["#{File.dirname(__FILE__)}/support/**/*.rb"].each { |file| require file }
