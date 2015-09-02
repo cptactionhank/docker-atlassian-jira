c7c7875b20559d5361d9ec51d2b76028dce16825
Circle CI Automated Builder <circleci@circleci>
2015-09-03 00:01:33 +0200

    Updated angular to have version specific configurations

--- javascripts/docker-configurator.js
+++ javascripts/docker-configurator.js
@@ -1,15 +1,38 @@
-
 var app = angular.module('docker-configurator', []);
 
 app.run(function($rootScope) {
 
+	// configuration constants for the various Atlassian JIRA docker images
+	// where each key defines the most recent version that had that specific
+	// configuration. For example the key 6.4.5 defines the most recent
+	// image that supported that configuration and finding a configuration for
+  // a specific image version the list of configurations should be sorted by
+  // key and then filtered by `<= [selected version]` and the largest key
+  // should then be used as the current configuration.
+	$rootScope.configurations = {
+		// for configurations <= 6.4.5
+		'6.4.5': {
+			home: '/var/local/atlassian/jira',
+			install: '/usr/local/atlassian/jira'
+		},
+		// define the default fall-back value of Docker image configuration
+		// settings, by using the highest available unicode character.
+		'\uDFFF': {
+			home: '/var/atlassian/jira',
+			install: '/opt/atlassian/jira'
+		}
+	}
+
 	$rootScope.jira = {
-		home: '/var/atlassian/jira',
-		install: '/opt/atlassian/jira'
-	};
+		home: 'not set',
+		install: 'not set'
+	}
 
 	$rootScope.update = function(value) {
-		console.log('selected value: ' + value);
+		var latest = Object.keys($rootScope.configurations).filter(function (item) { return String.naturalCompare(item, value) >= 0; })[0]
+		if (latest) {
+			angular.merge($rootScope.jira, $rootScope.configurations[latest]);
+		}
 	};
 
 });
@@ -22,7 +45,7 @@ app.controller('ConfigurationController', function($rootScope, $scope, $http) {
 	$http
 	.get('http://crossorigin.me/https://hub.docker.com/v2/repositories/cptactionhank/atlassian-jira/tags/?page_size=1000')
 	.success(function(data, status) {
-		$scope.tags = data.results;
+		$scope.tags = data.results.sort(function(a,b) { return String.naturalCompare(a.name, b.name); });
 		$scope.update($scope.jira.version);
 		$scope.status = "";
 	}).error(function(data, status) {
---
+++ javascripts/naturalSortVersion.js
@@ -0,0 +1,2 @@
+/*! litejs.com/MIT-LICENSE.txt */
+String.naturalCompare=function(f,g){function e(b,c,a){if(a){for(d=c;a=e(b,d),76>a&&65<a;)++d;return+b.slice(c-1,d)}a=l&&l.indexOf(b.charAt(c));return-1<a?a+76:(a=b.charCodeAt(c)||0,45>a||127<a)?a:46>a?65:48>a?a-1:58>a?a+18:65>a?a-11:91>a?a+11:97>a?a-37:123>a?a+5:a-63}var d,c,b=1,h=0,k=0,l=String.alphabet;if((f+="")!=(g+=""))for(;b;)if(c=e(f,h++),b=e(g,k++),76>c&&76>b&&66<c&&66<b&&(c=e(f,h,h),b=e(g,k,h=d),k=d),c!=b)return c<b?-1:1;return 0};
