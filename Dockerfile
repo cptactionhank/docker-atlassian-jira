f4ebb13a77899f3036a35c4c892105706a6bc47b
Martin Jensen <maage@dotmaage.dk>
2015-10-12 21:36:21 +0200

    Updated to JIRA Core EAP

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM airdock/oracle-jdk
+FROM java:8
 
 # Configuration variables.
 ENV JIRA_HOME     /var/atlassian/jira
@@ -12,10 +12,12 @@ RUN set -x \
     && apt-get install --quiet --yes --no-install-recommends libtcnative-1 xmlstarlet \
     && apt-get clean \
     && mkdir -p                "${JIRA_HOME}" \
+    && mkdir -p                "${JIRA_HOME}/caches/indexes" \
     && chmod -R 700            "${JIRA_HOME}" \
     && chown -R daemon:daemon  "${JIRA_HOME}" \
     && mkdir -p                "${JIRA_INSTALL}/conf/Catalina" \
-    && curl -Ls                "http://www.atlassian.com/software/jira/downloads/binary/atlassian-jira-core-${JIRA_VERSION}.tar.gz" | tar -xz --directory "${JIRA_INSTALL}" --strip-components=1 --no-same-owner \
+    && curl -Ls                "https://www.atlassian.com/software/jira/downloads/binary/atlassian-jira-core-${JIRA_VERSION}.tar.gz" | tar -xz --directory "${JIRA_INSTALL}" --strip-components=1 --no-same-owner \
+    && curl -Ls                "https://dev.mysql.com/get/Downloads/Connector-J/mysql-connector-java-5.1.36.tar.gz" | tar -xz --directory "${JIRA_INSTALL}/lib" --strip-components=1 --no-same-owner "mysql-connector-java-5.1.36/mysql-connector-java-5.1.36-bin.jar" \
     && chmod -R 700            "${JIRA_INSTALL}/conf" \
     && chmod -R 700            "${JIRA_INSTALL}/logs" \
     && chmod -R 700            "${JIRA_INSTALL}/temp" \
@@ -24,6 +26,7 @@ RUN set -x \
     && chown -R daemon:daemon  "${JIRA_INSTALL}/logs" \
     && chown -R daemon:daemon  "${JIRA_INSTALL}/temp" \
     && chown -R daemon:daemon  "${JIRA_INSTALL}/work" \
+    && sed --in-place          "s/java version/openjdk version/g" "${JIRA_INSTALL}/bin/check-java.sh" \
     && echo -e                 "\njira.home=$JIRA_HOME" >> "${JIRA_INSTALL}/atlassian-jira/WEB-INF/classes/jira-application.properties"
 
 # Use the default unprivileged account. This could be considered bad practice
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -7,12 +7,13 @@ REGEX_WARN    = /WARNING|WARN/
 REGEX_ERROR   = /ERROR|ERR/
 REGEX_SEVERE  = /SEVERE|FATAL/
 REGEX_STARTUP = /Server startup in \d+ ms/
-REGEX_FILTER  = Regexp.compile Regexp.union [
+REGEX_FILTER  = Regexp.compile (Regexp.union [
+  /org\.apache\.catalina\.loader\.WebappClassLoaderBase\.checkThreadLocalMapForLeaks\ The\ web\ application\ \[ROOT\]\ created\ a\ ThreadLocal\ with\ key\ of\ type\ \[.*\]\ \(value\ \[.*\]\)\ and\ a\ value\ of\ type\ \[.*\]\ \(value\ \[.*\]\)\ but\ failed\ to\ remove\ it\ when\ the\ web\ application\ was\ stopped\.\ Threads\ are\ going\ to\ be\ renewed\ over\ time\ to\ try\ and\ avoid\ a\ probable\ memory\ leak\./,
   # For some reason when setting up the database the indexing path is not set
   # yielding the following errors.
-  %r{\[c\.a\.jira\.upgrade\.ConsistencyCheckImpl\]\ Indexing\ is\ turned\ on,\ but\ index\ path\ \[null\]\ invalid\ \-\ disabling\ indexing},
-  %r{\[c\.a\.j\.issue\.index\.DefaultIndexManager\]\ File\ path\ not\ set\ \-\ not\ indexing}
-]
+  /\[c\.a\.jira\.upgrade\.ConsistencyCheckImpl\]\ Indexing\ is\ turned\ on,\ but\ index\ path\ \[null\]\ invalid\ \-\ disabling\ indexing/,
+  /\[c\.a\.j\.issue\.index\.DefaultIndexManager\]\ File\ path\ not\ set\ \-\ not\ indexing/
+])
 
 Dir["#{File.dirname(__FILE__)}/support/**/*.rb"].each { |file| require file }
 
@@ -57,8 +58,10 @@ RSpec.configure do |config|
 
   Capybara.configure do |conf|
     conf.register_driver :poltergeist_debug do |app|
-      Capybara::Poltergeist::Driver.new app,
-        timeout: timeout,
+      Capybara::Poltergeist::Driver.new app, timeout: timeout,
+                # we should't care about javascript errors since we did not make any
+        # implementation, but only deliver the software packages as best
+        # effort and this is more an Atlassian problem.
         js_errors: false,
         phantomjs_logger: Capybara::Poltergeist::Suppressor.new
     end
@@ -68,6 +71,9 @@ RSpec.configure do |config|
     conf.run_server = false
     conf.default_driver = :poltergeist_debug
     conf.default_wait_time = timeout
+
+    # conf.ignore_hidden_elements = false
+    # conf.visible_text_only = false
   end
 
   Docker::DSL.configure do |conf|
