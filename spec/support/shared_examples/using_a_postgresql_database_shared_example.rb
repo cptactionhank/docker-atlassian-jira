90623cd60a2bab0607b64abffea7b3b8913bbf29
Martin Aksel Jensen <maage@dotmaage.dk>
2016-02-06 22:27:16 +0100

    Updated CI configurations and acceptance tests

--- spec/support/shared_examples/using_a_postgresql_database_shared_example.rb
+++ spec/support/shared_examples/using_a_postgresql_database_shared_example.rb
@@ -10,7 +10,7 @@ shared_examples 'using a postgresql database' do
       # fill in database configuration
       # select "PostgreSQL", :from => 'databaseType'
       fill_in 'jira-setup-database-field-database-type-field', with: 'PostgreSQL'
-      fill_in 'jdbcHostname', with: $container_postgres.host
+      fill_in 'jdbcHostname', with: @container_db.host
       fill_in 'jdbcPort', with: '5432'
       fill_in 'jdbcDatabase', with: 'jiradb'
       fill_in 'jdbcUsername', with: 'postgres'
