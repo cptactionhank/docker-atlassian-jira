5d8b9a44e31b7981c4f3dee44d827bceb49f1841
Martin Aksel Jensen <maage@dotmaage.dk>
2016-04-04 21:39:36 +0200

    Stylistic refactoring of testing code and added proxy support

--- spec/support/shared_examples/using_an_embedded_database_shared_example.rb
+++ spec/support/shared_examples/using_an_embedded_database_shared_example.rb
@@ -1,8 +1,5 @@
-require 'timeout'
-require 'spec_helper'
-
 shared_examples 'using an embedded database' do
-	before :all do
+  before :all do
     within 'form#jira-setup-database' do
       choose 'jira-setup-database-field-database-internal'
       click_button 'Next'
