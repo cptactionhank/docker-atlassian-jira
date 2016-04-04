e380a5c12ba6658510cbd857549ff8f2d315b801
Martin Aksel Jensen <maage@dotmaage.dk>
2016-04-04 20:41:04 +0200

    Stylistic refactoring of testing code and added proxy support

--- spec/support/matchers/state_matcher.rb
+++ spec/support/matchers/state_matcher.rb
@@ -7,7 +7,7 @@ module Docker
     extend RSpec::Matchers::DSL
 
     class StateMatcher < RSpec::Matchers::BuiltIn::Include
-      alias_method :parent_matches?, :matches?
+      alias parent_matches? matches?
 
       def initialize(*expected)
         @expected = expected
