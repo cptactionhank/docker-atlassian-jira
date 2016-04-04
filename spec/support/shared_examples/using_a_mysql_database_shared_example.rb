2f7a63489f4f25348f3d0489d2feb6aaa2be142b
Martin Aksel Jensen <maage@dotmaage.dk>
2016-04-04 21:20:13 +0200

    Stylistic refactoring of testing code and added proxy support

--- spec/support/shared_examples/using_a_mysql_database_shared_example.rb
+++ spec/support/shared_examples/using_a_mysql_database_shared_example.rb
@@ -1,23 +1,19 @@
-require 'timeout'
-require 'spec_helper'
-
-shared_examples 'using a mysql database' do
-	before :all do
+shared_examples 'using a MySQL database' do
+  before :all do
     within 'form#jira-setup-database' do
-    	# select using external database
-    	choose 'jira-setup-database-field-database-external'
-			wait_for_page
-			# fill in database configuration
-    	# select "PostgreSQL", :from => 'databaseType'
-    	fill_in 'jira-setup-database-field-database-type-field', with: 'MySQL'
-    	fill_in 'jdbcHostname', with: @container_db.host
-    	fill_in 'jdbcPort', with: '3306'
-    	fill_in 'jdbcDatabase', with: 'jiradb'
-    	fill_in 'jdbcUsername', with: 'root'
-    	fill_in 'jdbcPassword', with: 'mysecretpassword'
-    	# continue database setup
+      # select using external database
+      choose 'jira-setup-database-field-database-external'
+      # allow some time for the DOM to change
+      sleep 1
+      # fill in database configuration
+      fill_in 'jira-setup-database-field-database-type-field', with: 'MySQL'
+      fill_in 'jdbcHostname', with: @container_db.host
+      fill_in 'jdbcPort', with: '3306'
+      fill_in 'jdbcDatabase', with: 'jiradb'
+      fill_in 'jdbcUsername', with: 'root'
+      fill_in 'jdbcPassword', with: 'mysecretpassword'
+      # continue database setup
       click_button 'Next'
-      wait_for_page
     end
   end
 end
