825099f3fa2dab1ee1b2cb2d76668eeffebc7086
Circle CI Automated Builder <circleci@circleci>
2016-05-04 22:29:28 +0000

    Created new Atlassian JIRA branch for version 7.1.6

---
+++ docker-entrypoint.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# check if the `server.xml` file has been changed since the creation of this
+# Docker image. If the file has been changed the entrypoint script will not
+# perform modifications to the configuration file.
+if [ "$(stat --format "%Y" "${JIRA_INSTALL}/conf/server.xml")" -eq "0" ]; then
+  if [ -n "${X_PROXY_NAME}" ]; then
+    xmlstarlet ed --inplace --pf --ps --insert '//Connector[@port="8080"]' --type "attr" --name "proxyName" --value "${X_PROXY_NAME}" "${JIRA_INSTALL}/conf/server.xml"
+  fi
+  if [ -n "${X_PROXY_PORT}" ]; then
+    xmlstarlet ed --inplace --pf --ps --insert '//Connector[@port="8080"]' --type "attr" --name "proxyPort" --value "${X_PROXY_PORT}" "${JIRA_INSTALL}/conf/server.xml"
+  fi
+  if [ -n "${X_PROXY_SCHEME}" ]; then
+    xmlstarlet ed --inplace --pf --ps --insert '//Connector[@port="8080"]' --type "attr" --name "scheme" --value "${X_PROXY_SCHEME}" "${JIRA_INSTALL}/conf/server.xml"
+  fi
+  if [ -n "${X_PATH}" ]; then
+    xmlstarlet ed --inplace --pf --ps --update '//Context/@path' --value "${X_PATH}" "${JIRA_INSTALL}/conf/server.xml"
+  fi
+fi
+
+exec "$@"
