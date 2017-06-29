375631e72cba54e1ecaf3ad197d872f79084f745
Circle CI Automated Builder <circleci@circleci>
2017-06-29 22:29:53 +0000

    Created new Atlassian JIRA branch for version 7.4.0

---
+++ spec/support/helpers/docker_helper.rb
@@ -0,0 +1,49 @@
+require 'docker'
+require 'uri'
+require 'rspec'
+require 'rspec/expectations'
+
+module Docker
+  class Container
+    def mapped_port(port)
+      port_string = port.first.reverse.join '/'
+      json['NetworkSettings']['Ports'][port_string].first['HostPort']
+    end
+
+    def host
+      json['NetworkSettings']['IPAddress']
+    end
+
+    def setup_capybara_url(port, path = '')
+      docker_url        = URI.parse Docker.url
+      docker_url.host   = 'localhost' # Docker.info['Name']
+      docker_url.scheme = 'http'
+      docker_url.path   = path
+      docker_url.port   = mapped_port port
+      Capybara.app_host = docker_url.to_s
+    end
+
+    def kill_and_wait(options = {})
+      options = { timeout: Docker::DSL.timeout }.merge(options)
+      kill options
+      wait options[:timeout]
+    end
+
+    def wait_for_output(regex)
+      thread = Thread.new do
+        timeout(Docker::DSL.timeout) do
+          Thread.handle_interrupt(TimeoutError => :on_blocking) do
+            streaming_logs stdout: true, stderr: true, follow: true do |_, chunk|
+              if chunk =~ regex
+                Thread.current[:chunk] = chunk
+                Thread.exit
+              end
+            end
+          end
+        end
+      end
+      thread.join
+      thread[:chunk].to_s.match(regex).to_a.drop 1
+    end
+  end
+end
