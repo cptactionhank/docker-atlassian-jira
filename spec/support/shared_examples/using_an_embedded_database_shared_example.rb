19e9e9b93e44bf45c35a0ce4d9ccce5341c08821
Martin Aksel Jensen <maage@dotmaage.dk>
2017-07-13 18:17:49 +0200

    Cleanup to keep only essential files

--- spec/support/shared_examples/using_an_embedded_database_shared_example.rb
+++
@@ -1,8 +0,0 @@
-shared_examples 'using an embedded database' do
-  before :all do
-    within 'form#jira-setupwizard' do
-      choose 'jira-setupwizard-database-internal'
-      click_button 'Next'
-    end
-  end
-end
